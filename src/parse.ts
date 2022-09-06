c5f34c4e730ef6054b7d8a423e47ae8b657ec9e4
Kyle Scott <kyscott@umich.edu>
2022-09-06 12:31:58 -0400

    without cli

--- src/parse.ts
+++ src/parse.ts
@@ -1,13 +1,27 @@
+import type { JsonFragment } from "@ethersproject/abi";
+import { Interface } from "@ethersproject/abi";
 import * as fs from "fs/promises";
 
 export const parse = async (): Promise<void> => {
-  // const rawFile = await fs.readFile("data/rebases.json");
+  const rawFile = await fs.readFile("abis/LiquidityGaugeV3.json");
 
-  // const rebases: Rebase[] = JSON.parse(rawFile.toString()) as Rebase[];
+  const jsonFile = JSON.parse(rawFile.toString()) as
+    | Interface
+    | { abi: Interface };
 
-  await fs.writeFile("data/pools.json", JSON.stringify(0, null, 2));
+  const abi = jsonFile instanceof Interface ? jsonFile : jsonFile.abi;
 
-  console.log(`Discovered and wrote ${0} pools`);
+  const f = JSON.parse(JSON.stringify(abi)) as JsonFragment[];
+
+  const out = f.map((m) => {
+    const { gas, ...rest } = m;
+    return { gas: gas?.toString(), rest };
+  });
+
+  await fs.writeFile(
+    "output/LiquidityGaugeV3.json",
+    JSON.stringify(out, null, 2)
+  );
 };
 
 parse().catch((err) => {
